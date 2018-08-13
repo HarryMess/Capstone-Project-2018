0a9c9a50838e3f971d52506926b72e56ee045005
s3488631 <@student.rmit.edu.au>
2018-08-13 18:01:22 +1000

    Updated the user class and added in some basic methods to allow testing

--- Capstone-Project-2018/src/user.java
+++ Capstone-Project-2018/src/user.java
@@ -1,4 +1,46 @@
 
-public class user {
-
+public class user 
+{
+	private string userName;
+	private string email;
+	private string password; 
+	
+	public user(string userName, string email, string password)
+	{
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+	}
+	
+	public string getUserName()
+	{
+		return username;
+	}
+	public void setUserName(string username)
+	{
+		this.userName = userName;
+	}
+	
+	public string getEmail()
+	{
+		return email;
+	}
+	public void setPassword(string email)
+	{
+		this.password = password;
+	}
+	
+	public string getPassword()
+	{
+		return password;
+	}
+	public void setPassword(string password)
+	{
+		this.password = password; 
+	}
+	
+	public string getString()
+	{
+		return "Username: " + username + "\n" + "Email: " + email + "\n" + "Password: " + password + "\n";
+	}
 }

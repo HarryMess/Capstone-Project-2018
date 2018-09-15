004c16b05441545d19c15a4077c1ed208312b68a
joshuaiaco <joshua.iaco@gmail.com>
2018-09-15 17:40:56 +1000

    Revert "Revert "Merge branch 'master' of https://github.com/HarryMess/Capstone-Project-2018""
    This reverts commit dadb03a4eb14bd875dab46197df34a4db8c5bacd.

--- Capstone-Project-2018/src/model/Admin.java
+++ Capstone-Project-2018/src/model/Admin.java
@@ -1,10 +1,32 @@
 package model;
 
-public class Admin extends User {
-
-	public Admin(String userName, String email, String password) {
-		super(userName, email, password);
+public class Admin extends User 
+{
+	
+	public Admin(String email, String password) {
+		super(email, password);
 		// TODO Auto-generated constructor stub
-	}	
+	}
+	
+	
+	// function report of user data
+	public void reportUserdate() 
+	{
+		
+	}
+	
+	// function notification to user
+	public void notification()
+	{
+		
+	}
+	
+	// function block user
+	public void blockUser()
+	{
+		
+	}
+	
+	
 	
 }

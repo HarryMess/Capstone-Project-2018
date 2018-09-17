9028b9c0f894215882fb78ca82d064ae60ce06dd
joshuaiaco <joshua.iaco@gmail.com>
2018-09-17 18:07:04 +1000

    loginMethod committing
    comitting loginMethod (in progress) need to seek verification

--- Capstone-Project-2018/src/controller/LoginListener.java
+++ Capstone-Project-2018/src/controller/LoginListener.java
@@ -2,6 +2,10 @@ package controller;
 
 import javax.swing.*;
 
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+
 import main.ConsoleApplication;
 import model.Model;
 import model.User;
@@ -13,11 +17,62 @@ public class LoginListener implements ActionListener
 {
 	private JFrame parent;
 	private JTextField userField, passField;
+	
 	public LoginListener(JFrame parent, JTextField userField, JTextField passField)
 	{
 		this.parent = parent;
 		this.userField = userField;
 		this.passField = passField;
+	}
+	
+	public void loginMethod (String email, String password)
+	{
+		
+		final String dbURL = "jdbc:derby:Database;create=true;user='s3488361;password=password"; // This might have to be changed, if you get a "No suitable driver found for ..." Error the classpath is wrong
+	    Connection connec = null; /* Instance */
+	    Statement statem = null;
+
+		{
+	        try
+	        {
+	            Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance(); // use org.apache.derby.jdbc.EmbeddedDriver
+	            connec = DriverManager.getConnection(dbURL);
+	        }
+
+	        catch (Exception exception)
+	        {
+	            exception.printStackTrace(); /* Calls the toString method of whatever exception was thrown */
+	        }
+
+	        try
+	        {
+	            statem = connec.createStatement();
+	            ResultSet res = statem.executeQuery("select * from users where email='"+email+"' AND password = '"+password+"'");
+	            int counter = 0;
+	            while (res.next())
+	            {
+	                counter += 1;
+	            }
+
+	            if (counter == 1)
+	            {
+	                System.out.println("Access granted");
+	            }
+
+	            else
+	            {
+	                System.out.println("Username and/or password do not match up with our records");
+	                System.out.println("Access denied");
+
+	            }
+	        }
+
+	        catch(Exception exception)
+	        {
+	            exception.printStackTrace();
+	        }
+	    }
+		
 	}
 
 	public void actionPerformed(ActionEvent e)
@@ -40,7 +95,10 @@ public class LoginListener implements ActionListener
 					"Authentication failed", JOptionPane.ERROR_MESSAGE);
 			
 		} else  {			
-			//loginMethod(email, password);
+			
+			
+			
+			loginMethod(email, password);
 			System.out.println("Test call.\n Email: " + email + "\nPassword: " + password);
 			
 			JOptionPane.showMessageDialog(null, "Login successul", "Login Confirmation",

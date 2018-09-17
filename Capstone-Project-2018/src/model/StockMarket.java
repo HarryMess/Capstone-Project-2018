45261f32b0c20ecab0ceeb7489af0e4a28751db4
Paul King <PaulKing09@bigpond.com>
2018-09-17 20:34:41 +1000

    Updates to StockMarket.java

--- Capstone-Project-2018/src/model/StockMarket.java
+++ Capstone-Project-2018/src/model/StockMarket.java
@@ -10,8 +10,7 @@ import java.util.List;
 public class StockMarket {
 
 	private static StockMarket stockMarket;	
-	
-	private User currentUser;	
+
 	private List<User> users;
 	private List<Company> companies;
 	private List<Stock> stocks;	
@@ -21,20 +20,20 @@ public class StockMarket {
 
 		if (stockMarket == null) {
 			Model model = Model.getInstance();
-			stockMarket = new StockMarket(model.getUsers(), model.getCompanies(),
-						  model.getStocks(), model.getTransactionHistory());
+			stockMarket = new StockMarket();
 		}
 		
 		return stockMarket;
 	}
 
-	private StockMarket(List<User> users, List<Company> companies, 
-			List<Stock> stocks, List<Transaction> transactionHistory) {
+	private StockMarket() {
 		
-		this.companies = companies;
-		this.stocks = stocks; // need to decide how we want to implement this
-		this.users = users;
-		this.transactionHistory = transactionHistory;
+		Model model = Model.getInstance();
+		
+		this.users = model.getUsers();
+		this.companies = model.getCompanies();
+		this.stocks = model.getStocks(); // need to decide how we want to implement this
+		this.transactionHistory =  model.getTransactionHistory();
 	}
 
 	// This method will be called each hour to update the prices in the stock market
@@ -43,33 +42,18 @@ public class StockMarket {
 		
 	}
 	
-	/**
-	 * 
-	 * @param account
-	 * @param stock
-	 */
-	public void buyStockFromMarket(TradingAccount account, Stock stock, int amount) {
-		// TODO - implement StockMarketSystem.buyStockFromMarket
+	public void buyStock(TradingAccount buyer, TradingAccount seller, int amount,
+			float pricePerStock) {
+		// TODO - implement StockMarketSystem.buyStock
 		
 	}
 
-	/**
-	 * 
-	 * @param sellAcount
-	 * @param buyAccount
-	 * @param stock
-	 */
 	public void sellStock(TradingAccount sellAccount, TradingAccount buyAccount, Stock stock, 
 			int amount, float pricePerStock) {
 		// TODO - implement StockMarketSystem.sellStock
 		
 	}
 
-	/**
-	 * 
-	 * @param sender
-	 * @param receiver
-	 */
 	public void transferFunds(int sender, int receiver, float amount) {
 		// TODO - implement StockMarketSystem.transferFunds
 		

febc3b9a33c756ae9484a5b934805fecb831e729
Paul King <s3449513@student.rmit.edu.au>
2018-10-10 23:40:00 +1100

    Updates to database schema

--- Capstone-Project-2018/sql/CreateTables.sql
+++ Capstone-Project-2018/sql/CreateTables.sql
@@ -1,26 +1,28 @@
 CREATE TABLE Users (
-	Email varchar (255) NOT NULL,
+	id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+	Email varchar (255) UNIQUE,
 	Password varchar (255) NOT NULL,
 	isAdmin BOOLEAN NOT NULL,
-	PRIMARY KEY (Email)
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE Trade_Accounts (
-	id int NOT NULL,
+	User_id int NOT NULL,
+	Email varchar (255),
 	Name varchar (50),
 	Balance float NOT NULL,
 	Hours_active int,
-	Email varchar (255),
 	PRIMARY KEY (id),
+	FOREIGN KEY (User_id) REFERENCES Users(id)
 	FOREIGN KEY (Email) REFERENCES Users(Email)
 );
 
 CREATE TABLE Stock (
 	Code varchar(5) NOT NULL,
 	Name varchar (255) NOT NULL,
-	Trade_Accountsid int NOT NULL,
-	PRIMARY KEY Code,
-	FOREIGN KEY (Trade_Accountsid) REFERENCES Trade_Accounts(id),
+	Owner_id int NOT NULL,
+	PRIMARY KEY (Code),
+	FOREIGN KEY (Owner_id) REFERENCES Trade_Accounts(id),
 );
 
 CREATE TABLE Stock_History(
@@ -32,14 +34,15 @@ CREATE TABLE Stock_History(
 	FOREIGN KEY (company_id) REFERENCES Stock(company_id)
 );
 
-CREATE TABLE Stock_Transaction(
+CREATE TABLE Transaction(
+	id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)
 	date_time TIMESTAMP NOT NULL,
 	buyer int NOT NULL,
 	seller int NOT NULL,
 	company_code varchar(5) NOT NULL,
 	price float NOT NULL,
 	amount int,
-	PRIMARY KEY (date_time, buyer, seller),
+	PRIMARY KEY (id),
 	FOREIGN KEY (buyer) REFERENCES Trade_Accounts(id),
 	FOREIGN KEY (seller) REFERENCES Trade_Accounts(id),
 	FOREIGN KEY (company_code) REFERENCES Stock(code)
@@ -50,6 +53,6 @@ CREATE TABLE Trade_History(
 	account_id int NOT NULL,
 	balance float NOT NULL,
 	share_value float NOT NULL,
-	PRIMARY KEY (date_time,account_id),
+	PRIMARY KEY (date_time, account_id),
 	FOREIGN KEY account_id REFERENCES Trade_Accounts(id)
 );
